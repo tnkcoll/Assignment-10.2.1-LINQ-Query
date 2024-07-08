0886b1a3e9d5698254fdcf16144ceb4553b0d1ea
tnkcoll <theodore.collins2023@outlook.com>
2024-07-08 13:28:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_10._2._1_LINQ_Query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 2, -1, 3, -3, 10, -200 };
+
+            var numQuery = from num in ints
+                           where num > 0 
+                           select num;
+
+            foreach (var num in numQuery)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
